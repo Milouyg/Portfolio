564ff9fe8ad39e166783072b0dfd00e62421d44c
Milouyg <33333@ma-web.nl>
2023-09-14 22:03:25 +0200

    Making a start with the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Component } from '@angular/core';
+import data from "../../../assets/json/info.json";
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements AfterViewInit  {
+    title = 'portfolio';
+    data;
+
+    constructor(){
+        this.data = data.nav; // We fetch the data here
+        console.log(this.data)
+    }
+    ngAfterViewInit(): void {
+
+    }
+
+
+}
