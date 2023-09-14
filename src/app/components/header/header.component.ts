775d8610f5ca1075daa71c126c987ebf77a642b4
Milouyg <33333@ma-web.nl>
2023-09-14 17:00:45 +0200

    Created header and it's responsive

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component } from '@angular/core';
+import data from "./../../../assets/json/info.json";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements AfterViewInit  {
+    title = 'portfolio';
+    data;
+
+    constructor(){
+        this.data = data.nav; // We fetch the data here
+        console.log(this.data)
+    }
+
+    ngAfterViewInit(): void {
+
+    }
+
+    openMenu(){
+        const menuItem = document.getElementById("mobile-menu");
+        menuItem?.classList.toggle("hidden");
+        console.log(menuItem)
+    }
+}
